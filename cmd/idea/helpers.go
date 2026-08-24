7ec4ac1485ed0147978043541d38cc3c448d0dab
fistos3rr <fistos3rr@gmail.com>
2026-08-24 16:32:23 +0300

    actions: autostart server

---
+++ cmd/idea/helpers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"maps"
+	"net/http"
+)
+
+type envelope map[string]any
+
+func (app *application) writeJSON(
+	w http.ResponseWriter,
+	status int,
+	data envelope,
+	headers http.Header,
+) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
--- cmd/idea/routes.go
+++ cmd/idea/routes.go
@@ -8,5 +8,8 @@ import (
 
 func (app *application) routes() http.Handler {
 	router := mux.NewRouter()
+	//test health
+	router.HandleFunc("/v1/health", app.healthcheckHandler).Methods("GET")
+
 	return router
 }
