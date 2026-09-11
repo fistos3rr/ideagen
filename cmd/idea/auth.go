9a645fa2b8e7d80f86008ae540f6072e36a5cfa8
fistos3rr <fistos3rr@gmail.com>
2026-09-11 16:48:15 +0300

    wip: swagger + dto

--- cmd/idea/auth.go
+++ cmd/idea/auth.go
@@ -13,6 +13,65 @@ import (
 	"github.com/fistos3rr/ideagen/internal/api/dto"
 )
 
+// registerUserHandler godoc
+//
+// @Summary Register user
+// @Tags auth
+// @Accept json
+// @Produce json
+// @Param request body dto.UserCredentials true "Credentials"
+// @Success 201 {object} dto.MessageResponse "User created"
+// @Failure 400 {object} dto.ErrorResponse "Bad request"
+// @Failure 422 {object} dto.ValidationErrorResponse "Validation failed"
+// @Failure 500 {object} dto.ErrorResponse "Internal server error"
+// @Router /auth/register [post]
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input dto.UserCredentials
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := &data.User{
+		Email: input.Email,
+	}
+
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	resp := dto.MessageResponse{
+		Message: "user created successfully",
+	}
+	err = app.writeJSON(w, http.StatusCreated, resp, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
 // loginUserHandler godoc
 //
 // @Summary Login user
--- cmd/idea/routes.go
+++ cmd/idea/routes.go
@@ -16,35 +16,35 @@ func (app *application) routes() http.Handler {
 	router.NotFoundHandler = http.HandlerFunc(app.notFoundResponse)
 	router.MethodNotAllowedHandler = http.HandlerFunc(app.methodNotAllowedResponse)
 
-	router.HandleFunc("/v1/health", app.healthcheckHandler).Methods("GET")//
+	router.HandleFunc("/v1/health", app.healthcheckHandler).Methods("GET")
 
-	router.HandleFunc("/v1/ask", app.requireUserRole("Admin", app.aiHandler)).Methods("POST")//
+	router.HandleFunc("/v1/ask", app.requireUserRole("Admin", app.aiHandler)).Methods("POST")
 
-	router.HandleFunc("/v1/types", app.requireUserRole("Admin", app.createTypeHandler)).Methods("POST")//
-	router.HandleFunc("/v1/types/{id}", app.requireUserRole("Admin", app.showTypeHandler)).Methods("GET")//
-	router.HandleFunc("/v1/types/{id}", app.requireUserRole("Admin", app.deleteTypeHandler)).Methods("DELETE")//
-	router.HandleFunc("/v1/types", app.requireUserRole("Admin", app.listTypesHandler)).Methods("GET")//
-	router.HandleFunc("/v1/types/{id}", app.requireUserRole("Admin", app.updateTypeHandler)).Methods("PATCH")//
+	router.HandleFunc("/v1/types", app.requireUserRole("Admin", app.createTypeHandler)).Methods("POST")
+	router.HandleFunc("/v1/types/{id}", app.requireUserRole("Admin", app.showTypeHandler)).Methods("GET")
+	router.HandleFunc("/v1/types/{id}", app.requireUserRole("Admin", app.deleteTypeHandler)).Methods("DELETE")
+	router.HandleFunc("/v1/types", app.requireUserRole("Admin", app.listTypesHandler)).Methods("GET")
+	router.HandleFunc("/v1/types/{id}", app.requireUserRole("Admin", app.updateTypeHandler)).Methods("PATCH")
 
-	router.HandleFunc("/v1/ideas", app.requireUserRole("Admin", app.createIdeaHandler)).Methods("POST")//
-	router.HandleFunc("/v1/ideas/{id}", app.requireUserRole("Admin", app.showIdeaHandler)).Methods("GET")//
-	router.HandleFunc("/v1/ideas/{id}", app.requireUserRole("Admin", app.deleteIdeaHandler)).Methods("DELETE")//
-	router.HandleFunc("/v1/ideas", app.requireUserRole("Admin", app.listIdeasHandler)).Methods("GET")//
-	router.HandleFunc("/v1/ideas/{id}", app.requireUserRole("Admin", app.updateIdeaHandler)).Methods("PATCH")//
+	router.HandleFunc("/v1/ideas", app.requireUserRole("Admin", app.createIdeaHandler)).Methods("POST")
+	router.HandleFunc("/v1/ideas/{id}", app.requireUserRole("Admin", app.showIdeaHandler)).Methods("GET")
+	router.HandleFunc("/v1/ideas/{id}", app.requireUserRole("Admin", app.deleteIdeaHandler)).Methods("DELETE")
+	router.HandleFunc("/v1/ideas", app.requireUserRole("Admin", app.listIdeasHandler)).Methods("GET")
+	router.HandleFunc("/v1/ideas/{id}", app.requireUserRole("Admin", app.updateIdeaHandler)).Methods("PATCH")
 
-	router.HandleFunc("/v1/service/useridea", app.requireUserRole("Admin", app.createUserIdeaHandler)).Methods("POST")//
-	router.HandleFunc("/v1/service/me", app.requireAuthenticatedUser(app.showMeHandler)).Methods("GET")//
-	router.HandleFunc("/v1/service/ideas", app.requireAuthenticatedUser(app.listMyIdeasHandler)).Methods("GET")//
-	router.HandleFunc("/v1/service/ideas/{id}", app.requireAuthenticatedUser(app.showMyIdeaHandler)).Methods("GET")//
-	router.HandleFunc("/v1/service/ideas/{id}", app.requireAuthenticatedUser(app.deleteMyIdeaHandler)).Methods("DELETE")//
+	router.HandleFunc("/v1/service/useridea", app.requireUserRole("Admin", app.createUserIdeaHandler)).Methods("POST")
+	router.HandleFunc("/v1/service/me", app.requireAuthenticatedUser(app.showMeHandler)).Methods("GET")
+	router.HandleFunc("/v1/service/ideas", app.requireAuthenticatedUser(app.listMyIdeasHandler)).Methods("GET")
+	router.HandleFunc("/v1/service/ideas/{id}", app.requireAuthenticatedUser(app.showMyIdeaHandler)).Methods("GET")
+	router.HandleFunc("/v1/service/ideas/{id}", app.requireAuthenticatedUser(app.deleteMyIdeaHandler)).Methods("DELETE")
 
-	router.HandleFunc("/v1/service/idea/generate", app.requireAuthenticatedUser(app.generateMyIdeaHandler)).Methods("POST")//
-	router.HandleFunc("/v1/service/idea/buffer/{id}", app.requireAuthenticatedUser(app.showMyBufferIdeaHandler)).Methods("GET")//
-	router.HandleFunc("/v1/service/idea/buffer", app.requireAuthenticatedUser(app.listMyBufferIdeasHandler)).Methods("GET")//
-	router.HandleFunc("/v1/service/idea/buffer", app.requireAuthenticatedUser(app.chooseMyBufferIdeaHandler)).Methods("POST")//
+	router.HandleFunc("/v1/service/idea/generate", app.requireAuthenticatedUser(app.generateMyIdeaHandler)).Methods("POST")
+	router.HandleFunc("/v1/service/idea/buffer/{id}", app.requireAuthenticatedUser(app.showMyBufferIdeaHandler)).Methods("GET")
+	router.HandleFunc("/v1/service/idea/buffer", app.requireAuthenticatedUser(app.listMyBufferIdeasHandler)).Methods("GET")
+	router.HandleFunc("/v1/service/idea/buffer", app.requireAuthenticatedUser(app.chooseMyBufferIdeaHandler)).Methods("POST")
 
-	router.HandleFunc("/v1/register", app.registerUserHandler).Methods("POST")
-	router.HandleFunc("/v1/auth/login", app.loginUserHandler).Methods("POST")//
+	router.HandleFunc("/v1/auth/register", app.registerUserHandler).Methods("POST")
+	router.HandleFunc("/v1/auth/login", app.loginUserHandler).Methods("POST")
 	router.HandleFunc("/v1/auth/logout", app.requireAuthenticatedUser(app.logoutHandler)).Methods("POST")
 	router.HandleFunc("/v1/auth/refresh", app.refreshHandler).Methods("POST")
 
