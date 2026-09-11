f5702e2d4882ba54aeaec359eac92a06e3fa4e04
fistos3rr <fistos3rr@gmail.com>
2026-09-11 11:35:08 +0300

    wip: swagger + dto

--- internal/api/dto/errors.go
+++ internal/api/dto/errors.go
@@ -5,5 +5,5 @@ type ErrorResponse struct {
 }
 
 type ValidationErrorResponse struct {
-	Error map[string]string `json:"error" example:"{\"email\":\"must be a valid email address\"}"`
+	Error map[string]string `json:"error" example:{"email":"must be a valid email address"}`
 }
