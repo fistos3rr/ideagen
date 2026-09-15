0346ced5316aa3cc20663fe2c1393982cbbda2fd
fistos3rr <fistos3rr@gmail.com>
2026-09-15 14:14:59 +0300

    frontend base

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
-
-module.exports = {
   allowedDevOrigins: ['ideagen.x4nyx.space'],
-}
+};
 
 export default nextConfig;
